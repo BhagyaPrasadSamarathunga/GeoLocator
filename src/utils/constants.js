5cd522e1351162ef768a2260470b9abc9b600fbd
Chamika Hasanthi <>
2023-12-05 00:44:22 +0530

    initial commit

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
 export const DELTA = 0.001;
-export const DEFAULTLATITUDE = 52.533896;
-export const DEFAULTLONGITUDE = 13.417111;
+export const DEFAULTLATITUDE = 52.5200;
+export const DEFAULTLONGITUDE = 13.4050;
 export const OPENSTREETMAP_BASEURL = 'https://www.openstreetmap.org/api/0.6/map?';
